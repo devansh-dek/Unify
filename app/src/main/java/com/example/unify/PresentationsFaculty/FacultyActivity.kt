43d33f979da5e0ed4418ed40ee00c1625883ca34
Devansh <devanshksg@gmail.com>
2024-04-20 01:02:52 +0530

    Added Attendace for facualty activities

--- app/src/main/java/com/example/unify/PresentationsFaculty/FacultyActivity.kt
+++ app/src/main/java/com/example/unify/PresentationsFaculty/FacultyActivity.kt
@@ -1,12 +1,31 @@
 package com.example.unify.PresentationsFaculty
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.unify.R
+import com.example.unify.Utils.POST_FOLDER
+import com.example.unify.databinding.ActivityFacultyBinding
+import com.example.unify.databinding.ActivityPostsBinding
+import uploadImage
 
 class FacultyActivity : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityFacultyBinding.inflate(layoutInflater)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_faculty)
+
+        binding.attendance.setOnClickListener {
+            startActivity(Intent(this,AttendanceActivity::class.java))
+        }
+        setContentView(binding.root)
+
+
+
     }
 }
